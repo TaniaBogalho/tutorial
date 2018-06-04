d6261a68daf9ad052d841153030d910c4486152a
TaniaBogalho <39596411+TaniaBogalho@users.noreply.github.com>
2018-06-04 16:28:50 +0100

    Update CalculoSoma.java

--- src/main/java/com/exemplo/CalculoSoma.java
+++ src/main/java/com/exemplo/CalculoSoma.java
@@ -2,28 +2,28 @@ package com.exemplo;
 
 public class CalculoSoma {
 
-	float num1;
-	float num2;
+	Float num1;
+	Float num2;
 
-	public float getNum1() {
+	public Float getNum1() {
 		return num1;
 	}
 
-	public void setNum1(float num1) {
+	public void setNum1(Float num1) {
 		this.num1 = num1;
 	}
   
-	public float getNum2() {
+	public Float getNum2() {
 		return num2;
 	}
 
-	public void setNum1(float num2) {
+	public void setNum1(Float num2) {
 		this.num2 = num2;
 	}
 
 	@Override
 	public String toString() {
-    		float total = num1 + num2;
+    		Float total = num1 + num2;
   
 		//return "Track [title=" + title + ", singer=" + singer + "]";
     		return "CalculoSoma [num1=" + num1 + " + num2=" + num2 + " = " + total]";
