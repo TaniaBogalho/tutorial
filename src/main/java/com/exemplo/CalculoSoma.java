9aa17755ac58a6eb9d1831889006460aa38822aa
TaniaBogalho <39596411+TaniaBogalho@users.noreply.github.com>
2018-06-04 16:06:31 +0100

    Update CalculoSoma.java

--- src/main/java/com/exemplo/CalculoSoma.java
+++ src/main/java/com/exemplo/CalculoSoma.java
@@ -23,7 +23,7 @@ public class Calculo {
 
 	@Override
 	public String soma() {
-    		float total = num + num2;
+    		float total = num1 + num2;
   
 		//return "Soma [num1=" + num1 + ", num2=" + num2 + "]";
     		return "[" + num1 + ", "+", " + num2 + ", " = " + total]";
